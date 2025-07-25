ba2be0e730de9720960da5b739ebca9c9d91589d
JJ-Assassing <151294601+JJ-Assassing@users.noreply.github.com>
2025-07-25 08:36:37 -0400

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,10 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("La página se cargó correctamente.");
-  
-  const loginButton = document.querySelector(".login-button");
-  if (loginButton) {
-    loginButton.addEventListener("click", () => {
-      console.log("Iniciando sesión con Spotify...");
-    });
-  }
-});
+
